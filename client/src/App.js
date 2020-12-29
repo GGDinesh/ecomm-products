6b7f94029ca1f6ea92281e56073f0e19d6cc259e
Dinesh <ggdinesh594@gmail.com>
2020-12-29 15:04:07 +0530

    screen ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import Footer from './components/Footer';
+import Header from './components/Headers';
+import { Container } from 'react-bootstrap';
+import Homescreen from './screens/Homescreen';
 const App = () => {
   return (
     <div className="App">
-      <h1>Staring my MERN</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Homescreen />
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
